48ee26e1919397a5be6fc6ed1911430accaf00ba
Harshitha22594 <147131836+Harshitha22594@users.noreply.github.com>
2024-12-05 11:13:10 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package java_class;
+import java.util.*;
+public class Demo {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter the number: ");
+		int n = sc.nextInt();
+		System.out.print("the number: " + n);
+	}
+}
