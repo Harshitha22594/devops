e8ecba629cefcdf3e0d314f8cbccf22c6145125c
Harshitha22594 <147131836+Harshitha22594@users.noreply.github.com>
2024-12-12 11:11:49 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,5 @@
-import java.util.*;
 class Demo{
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("enter the number: ");
-		int n = sc.nextInt();
-		System.out.print("the number: " + n);
+		System.out.println("Hello");
 	}
 }
